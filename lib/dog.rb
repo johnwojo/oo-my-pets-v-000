0c18b276357c751875a864af74cfcc38a5b3e863
John Wojciechowski <jwojo13@gmail.com>
2017-12-11 23:26:56 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,9 @@
 class Dog
   attr_accessor :mood
   attr_reader :name
-  @@all_dogs = []
 
     def initialize(name)
       @name = name
       @mood = "nervous"
-      @@all_dogs << self
     end
 end
